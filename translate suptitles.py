2db53a84b7c59e61e5534c59cf85fc79f012f2de
darlinf <zkiduxi@gmail.com>
2022-07-23 21:02:07 -0400

    restructured the script

--- translate suptitles.py
+++ translate suptitles.py
@@ -1,20 +1,18 @@
 from tkinter import *
-from tkinterdnd2 import *
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import time
+from tkinterdnd2 import * 
 import re
 import os
+import pyperclip
+import os 
 
 
-def createGUI():
+def createGUI(functionFilePath):
     ws = TkinterDnD.Tk()
     ws.title('PythonGuides')
     ws.geometry('400x300')
     ws.config(bg='#fcb103')
     ws.drop_target_register(DND_FILES)
-    ws.dnd_bind('<<Drop>>', show_text)
+    ws.dnd_bind('<<Drop>>', functionFilePath)
 
     frame = Frame(ws)
     frame.pack()
@@ -24,13 +22,22 @@ def createGUI():
 def show_text(event):    
     filePath = event.data 
     timeLine = removeArrow(filePath)
-    textSpanish = trasnlate()
-    unionString(timeLine, textSpanish, filePath)
 
+    #Copy in clipboard
+    pyperclip.copy(textFromFile("suptitles/English.txt"))
+    pyperclip.paste()
+     
+    def show_text2 (event):
+        filePath2 = event.data[1:len(event.data)-1]
 
-def removeArrow(filePath): 
-    file = open(filePath, "r") 
-    splitText = re.split("\n", file.read().replace("<", "b"))
+        textSpanish = textFromFile(filePath2).split("\n")
+        unionString(timeLine, textSpanish, filePath)
+        
+    createGUI(show_text2)
+
+    
+def removeArrow(filePath):  
+    splitText = re.split("\n", textFromFile(filePath).replace("<", "b"))
     
     if(splitText[0] != "WEBVTT"):
         splitText.insert(0, "WEBVTT \n \n") 
@@ -50,35 +57,18 @@ def removeArrow(filePath):
         if "-->" not in i and "WEBVTT" not in i: 
             resultSuptitle += i + '\n' 
         elif i not in "WEBVTT": 
-            timeLine.append(i.replace(",", ".")) 
+            timeLine.append(i.replace(",", "."))  
 
-    file = open("suptitles/English.txt", "a")
-    file.write(resultSuptitle)
-    file.close() 
+    saveTextInFile("suptitles/English.txt", resultSuptitle)
 
     return timeLine
 
 
-def trasnlate():
-    PATH = "./chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
-    driver.get("https://translate.google.com/?op=docs") 
 
-    driver.find_element_by_xpath("/html/body/c-wiz/div/div[2]/c-wiz/div[3]/c-wiz/div[1]/c-wiz/div[1]/c-wiz/div[5]/div/div[2]/div/div/span/button[2]/span[3]").click()
-
-    upload_file = driver.find_element_by_xpath("/html/body/c-wiz/div/div[2]/c-wiz/div[3]/c-wiz/div[2]/c-wiz/div/div/form/div[1]/div[3]/input")
-    upload_file.send_keys("C:/Users/unknow/Desktop/translate-subtitles-automatic-python-main (1)/translate-subtitles-automatic-python-main/suptitles/English.txt")
-
-    driver.find_element_by_xpath("/html/body/c-wiz/div/div[2]/c-wiz/div[3]/c-wiz/div[2]/c-wiz/div/div/form/div[2]/div[2]/div/button").click()
-    
-    return driver.find_element_by_xpath("/html/body/pre").text.split("\n")
     
    
 def unionString(timeLine, splitTextSpanish, filePathResurt):
-    
-    file = open("suptitles/English.txt", "r")
-    splitTextEnglish = re.split("\n", file.read())
-    file.close() 
+    splitTextEnglish = re.split("\n", textFromFile("suptitles/English.txt")) 
     os.remove("suptitles/English.txt")
 
     unionResult = "WEBVTT \n \n" + timeLine[0] + "\n"
@@ -91,14 +81,25 @@ def unionString(timeLine, splitTextSpanish, filePathResurt):
         elif jT < len(timeLine):
             unionResult += "\n" + timeLine[jT] + "\n"
             jT += 1
-        j += 1 
+        j += 1  
 
-    file = open(filePathResurt + ".vtt", "a", encoding="utf-8")
-    file.write(str(unionResult))
+    filePathResurt = filePathResurt + ".vtt"
+    saveTextInFile(filePathResurt, str(unionResult))
+
+
+#Helpers function
+def saveTextInFile(path, textToSave):
+    file = open(path, "a", encoding="utf8")
+    file.write(textToSave)
     file.close() 
+    
+def textFromFile(path):
+    file = open(path, "r", encoding="utf8")
+    fileText = file.read()
+    file.close()
+    return fileText
 
-
-#Start program
-createGUI()
+#Start program, encoding='cp856', errors='ignore'
+createGUI(show_text)
 
 
